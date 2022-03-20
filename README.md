272a9bdb27007dafb2cf2c4326818740c58b3815
daianalonso <daalonso@dc.uba.ar>
2022-03-20 01:59:31 -0300

    descarga de archivos

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# alkemy-python
- 
+# Challenge Alkemy Data Analytics - Python 
+
+## Instrucciones: 
+
+1. Clonar este repositorio 
+
+```
+git clone https://github.com/daianalonso/alkemy-python
+```
+
+2. Crear un entorno virtual con python3
+
+```
+python3 -m venv tp2
+source tp2/bin/activate
+```
+
+3. Instalar las dependencias
+
+```
+pip install -r requirements.txt
+```
+
+3. Configurar la conexión con la base de datos
+
+4. Ejecutar 
+
+```
+python3 
+```
---
+++ downloads.py
@@ -0,0 +1,41 @@
+import os 
+import requests
+from url import *
+from datetime import date
+
+meses = {1:'enero', 2:'febrero', 3:'marzo', 4:'abril', 5:'mayo', 6:'junio', 7:'julio', 8:'agosto', 9:'septiembre', 10:'octubre',
+        11:'noviembre', 12:'diciembre'}
+
+# Almaceno la fecha de descarga de los archivos
+hoy = date.today()
+
+def descargar_csv(url, categoria):
+    '''
+    Descarga el csv desde la url pasada por parámetro, guardándolo en la ruta de archivo correspondiente a su categoría y
+    fecha de descarga.
+    '''
+    # Especifico la carpeta donde guardar la descarga
+    carpeta = categoria + '/' + str(hoy.year) + '-' + meses[hoy.month]
+    
+    # Si no existe la carpeta, la creo
+    if not os.path.exists(carpeta):
+            os.makedirs(carpeta)
+    
+    # Creo la ruta con el nombre de archivo 
+    filename = carpeta + '/' + categoria + '-' + str(hoy.day) + '-' + str(hoy.month) + '-' + str(hoy.year) + '.csv'
+
+    # Descargo el archivo y lo guardo en esa ruta
+    req = requests.get(url)
+    url_content = req.content
+    csv_file = open(filename, 'wb')
+    csv_file.write(url_content)
+    csv_file.close()
+
+
+if __name__ == "__main__":
+    try:
+        descargar_csv(URL_BIBLIOTECAS, 'bibliotecas')
+        descargar_csv(URL_MUSEOS, 'museos')
+        descargar_csv(URL_CINES, 'cines')
+    except Exception as e:
+        print(f'Hubo un error al realizar la descarga: {e}')
